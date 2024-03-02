c9819d75697428572da084d51d18f8039474aaf1
AlexandreGNT <stoapofftrack@gmail.com>
2024-03-02 20:19:04 +0100

    all etxh with prompt

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const grid_container = document.querySelector('#grid_container')
-
-function createGrid(container){
-    for(i=0;i<9;i++){
+function isInteger(number) {
+    return number === Math.round(number);
+  }
+function createGrid(container,numberCells){
+    numberCells = prompt('Number of cells')
+    const rootNumber = Math.pow(numberCells, 0.5)
+    let checkRootNumber = isInteger(rootNumber)
+    if(checkRootNumber){
+        for(i=0;i<numberCells;i++){
+            grid_container.setAttribute('style', `grid-template-columns: repeat(${rootNumber}, 1fr)`);
             const cell  = document.createElement('div')
             cell.classList.add('cell')
             container.appendChild(cell)
-    }
+            cell.addEventListener('mouseenter',()=> {
+                cell.classList.add('hovered')
+            })
+        }} else {
+            alert('Invalid input. Please enter a valid integer to create a perfect .');
+        }
+    
 }
-
 createGrid(grid_container)
